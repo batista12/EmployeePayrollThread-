ce28861b7e8dab6bf3a1ac90202c5e3b07fe728c
batista12 <50904472+batista12@users.noreply.github.com>
2020-11-06 22:13:48 -0800

    [Manasi]Employee Payroll Thread is created

---
+++ EmployeePayrollThread/src/main/java/com/capgemini/employeepayrollthread/EmployeePayrollThread.java
@@ -0,0 +1,13 @@
+package com.capgemini.employeepayrollthread;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmployeePayrollThread 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ EmployeePayrollThread/src/test/java/com/capgemini/employeepayrollthread/EmployeePayrollThreadTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.employeepayrollthread;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmployeePayrollThreadTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
